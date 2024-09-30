213903a83ea964d0ffb25be241ad6ef714c7c197
ameliabaldwin <nbaldwin9870@gmail.com>
2024-09-30 11:23:57 -0700

    Learning03 completed

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f = new Fraction();
+        int quit = 0;
+        string response = "";
+        while(quit == 0)
+        {
+            Console.Write("What is the top number: ");
+            int top = int.Parse(Console.ReadLine());
+
+            Console.Write("What is the bottom number: ");
+            int bottom = int.Parse(Console.ReadLine());
+
+            f.SetTop(top);
+            f.SetBottom(bottom);
+
+            Console.WriteLine(f.GetFractionString());
+            Console.WriteLine(f.GetDecimalValue());
+            Console.Write("Want to continue(Yes or No): ");
+            response = Console.ReadLine();
+            if (response == "No")
+            {
+                quit = 1;
+            }
+            else
+            {
+                Console.WriteLine("Okie dokie, continuing the program...");
+            }
+        }
+        
+        
     }
 }
