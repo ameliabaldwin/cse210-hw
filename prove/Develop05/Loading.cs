5dd65ea18bf7cd9bc1bc0c52dd28524a4c6c4330
ameliabaldwin <nbaldwin9870@gmail.com>
2024-10-16 10:33:54 -0700

    dev minful2

--- prove/Develop05/Loading.cs
+++ prove/Develop05/Loading.cs
@@ -1,6 +1,55 @@
 using System;
 public class Loading
 {
-    
+    private int counter;
+    public Loading()
+    {
+        counter = 0;
+    }
+    public void Sleep(int time)
+    {
+        System.Threading.Thread.Sleep(time);
+    }
+    public void Loadingdots()
+    {
+        counter++;
+        switch (counter % 4)
+        {
+            case 0: Console.Write("/"); break;
+            case 1: Console.Write("-"); break;
+            case 2: Console.Write("\\"); break;
+            case 3: Console.Write("|"); break;
+        }
+         Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
+    }
+    public void countdown()
+    {
+        for (int i = 5; i>0; i--)
+        {
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write($"Beginning in...{i}");
+            Sleep(2000);
+        }
+        Console.SetCursorPosition(0, Console.CursorTop);
+        Console.Clear();
+        Console.WriteLine("\n--Starting now!--");
+        
+    }
+    public void menu(string newNames)
+    {
+        Console.WriteLine("--The Main Menus of Menu--\n");
+        Sleep(200);
+        Console.WriteLine("1. Breathing Activity: Will help you blow down your enemies\n");
+        Sleep(200);
+        Console.WriteLine("2. Reflection Activity: Will teach you how to use the reflection of the sun to burn holes through the earth\n");
+        Sleep(200);
+        Console.WriteLine("3. Listing Activity: Have too many girl or boy friends \nphone numbers then pick this one, it will help you list all of them\n");
+        Sleep(200);
+        Console.WriteLine("4. Quit: this quits and shuts down the program");
+        Sleep(200);
+        Console.WriteLine("**DISCLAIMER: WE CAN'T PROMISE THAT ANY OF THESE THINGS WILL ACTUALLY DO WHAT THEY SAY AND THEY MIGHT CAUSE IREVERSABLE DAMAGE TO YOUR FUNKY SOUL**\n");
+        Sleep(200);
+        Console.Write($"okay let's keep this train going, {newNames} pick one of these options...:");
+    }
     
 }
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -33,8 +33,6 @@ class Program
             l.menu(newNames);
             string activity = Console.ReadLine();
             l.Sleep(200);
-
-            Console.WriteLine("They do not pay me enough for this job... ugh minimum wage I hate it.");
             int interval = 1;
             System.Timers.Timer timer = new System.Timers.Timer(interval);
             if (activity == "1")
@@ -134,18 +132,22 @@ class Program
 
             if (activity == "3")
             {
+                
                 string[] listPrompts = "Who are people that you appreciate?|What are personal strengths of yours?|Who are people that you have helped this week?|When have you felt the Holy Ghost this month?|Who are some of your personal heroes?".Split('|');
                 
                 Console.WriteLine("How Long are you gonna do this for(seconds): ");
                 int timeLength = int.Parse(Console.ReadLine());
                 
                 Listing li = new Listing(listPrompts, "Listing Activity", "This activity will help you list all your fears and desires for real, no cap", timeLength, timeLength);
+
+                int totalDurationInMillis = li.GetDurationInMilSecs();
+                int elapsedTime = 0;
+                
+                Console.WriteLine("Welcome to the Listing actitvity I hope you just have the best time in this activity...");
+
                 Console.WriteLine(li.GetActivityDetails());
                 
                 l.countdown();
-                
-                int totalDurationInMillis = li.GetDurationInMilSecs();
-                int elapsedTime = 0;
 
                 Console.WriteLine(li.GetRandomListing(rnd));
 
@@ -176,6 +178,7 @@ class Program
             if(activity == "4")
             {
                 quit = true;
+                Console.WriteLine("They do not pay me enough for this job... ugh minimum wage I hate it.");
             }
         }
     }
