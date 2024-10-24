e4a1f558249e7115b4028cff2d43c0bb898ce004
ameliabaldwin <nbaldwin9870@gmail.com>
2024-10-24 08:03:41 -0700

    Learning 6 shapes completed

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        Square sShape = new Square(2, "brown");
+        Rectangle rShape = new Rectangle(5, 6, "red");
+        Circle cShape = new Circle(7, "green");
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(sShape);
+        shapes.Add(rShape);
+        shapes.Add(cShape);
+
+        
+        foreach(Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            string color = shape.GetColor();
+            Console.WriteLine($"Color:{color}\nArea:{area}");
+        }
+        
+
     }
 }
